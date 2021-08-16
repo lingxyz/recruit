8fe4af923d6982d815874952abc6c62b741829bb
Ling Z <771152260@qq.com>
2021-08-16 11:22:16 +0800

    arch: vue3+ts+pug+stylus+element-plus

---
+++ web/src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+DeaultLayout
+  router-view
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import DeaultLayout from './layouts/DeaultLayout.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    DeaultLayout
+  }
+})
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
