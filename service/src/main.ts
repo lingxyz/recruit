8c76ede6ee840e0109f5e9c5e031abd7845c43ec
Ling Z <771152260@qq.com>
2021-08-16 14:03:35 +0800

    arch: 统一响应格式拦截器

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './interceptors/transform.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 绑定拦截器：响应映射
+  app.useGlobalInterceptors(new TransformInterceptor());
   const port = 3001;
   await app.listen(port);
   console.log(`server start at: http://localhost:${port}`)
