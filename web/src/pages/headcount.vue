453aac473502a95ec4e1041e1db6dfc544a8d908
Ling Z <771152260@qq.com>
2021-08-16 16:12:43 +0800

    feat: 查询招聘需求

--- web/src/pages/headcount.vue
+++ web/src/pages/headcount.vue
@@ -12,35 +12,37 @@ el-row
   //- el-col(:span='2' :offset="2")
   //-   el-button(type='info' plain) 确认搜索
 el-table(:data='tableData')
-  el-table-column(prop='address' label='岗位类型')
-  el-table-column(prop='address' label='职级')
-  el-table-column(prop='address' label='项目')
-  el-table-column(prop='address' label='原因')
-  el-table-column(prop='address' label='负责人')
-  el-table-column(prop='address' label='优先级')
-  el-table-column(prop='address' label='状态')
-  el-table-column(prop='address' label='一轮候选人')
-  el-table-column(prop='address' label='二轮候选人')
-  el-table-column(prop='address' label='待入职候选人')
+  el-table-column(prop='post' label='岗位类型')
+  el-table-column(prop='level' label='职级')
+  el-table-column(prop='project' label='项目')
+  el-table-column(prop='reason' label='原因')
+  el-table-column(prop='head' label='负责人')
+  el-table-column(prop='priority' label='优先级')
+  el-table-column(prop='status' label='状态')
+  el-table-column(prop='firstRound' label='一轮候选人')
+  el-table-column(prop='secondRound' label='二轮候选人')
+  el-table-column(prop='toBeEmployed' label='待入职候选人')
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, getCurrentInstance, reactive, onMounted } from 'vue'
+import statusVue from './status.vue';
 
 export default defineComponent({
-  name: 'App',
+  name: 'Headcount',
   setup() {
-    const item = {
-      date: '2016-05-02',
-      name: '王小虎',
-      address: 'XXX',
-    };
-    const tableData = ref(Array(20).fill(item));
+    const state = reactive({
+      tableData: [],
+      name: ''
+    });
 
-    return {
-      tableData,
-      name: ref('')
-    };
+    const { proxy } = getCurrentInstance();
+    (async () => {
+      const res = await proxy.$axios.get('http://localhost:3001/headcount')
+      state.tableData = res.data.data
+    })();
+
+    return state;
   }
 })
 </script>
