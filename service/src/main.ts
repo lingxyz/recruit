570278ec3cc428a23526abce421b383da19af74d
Ling Z <771152260@qq.com>
2021-08-16 15:09:17 +0800

    arch: 开启跨域

--- service/src/main.ts
+++ service/src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 绑定拦截器：响应映射
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 开启跨域
+  app.enableCors();
   const port = 3001;
   await app.listen(port);
   console.log(`server start at: http://localhost:${port}`)
